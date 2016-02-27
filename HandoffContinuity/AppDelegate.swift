0ca82f9b34f0ea1f58f2bb5487c8b10f4a921112
Henrique Valcanaia <henrique.valcanaia@hotmail.com>
2016-02-27 17:22:21 -0300

    Initial Commit

---
+++ HandoffContinuity/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HandoffContinuity
+//
+//  Created by Henrique Valcanaia on 2/27/16.
+//  Copyright © 2016 Henrique Valcanaia. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HandoffContinuity/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HandoffContinuity
+//
+//  Created by Henrique Valcanaia on 2/27/16.
+//  Copyright © 2016 Henrique Valcanaia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
