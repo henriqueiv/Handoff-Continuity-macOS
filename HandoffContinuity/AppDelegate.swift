33bfb422cb5c75ba0860b92472433cf3c5abd3a5
Henrique Valcanaia <henriqueindalencio@gmail.com>
2019-01-10 21:21:56 -0200

    Commit before backup

--- HandoffContinuity/AppDelegate.swift
+++ HandoffContinuity/AppDelegate.swift
@@ -20,6 +20,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
+    
+    func application(application: NSApplication, continueUserActivity userActivity: NSUserActivity, restorationHandler: ([AnyObject]) -> Void) -> Bool {
+        switch NSApplication.sharedApplication().windows[0].contentViewController {
+        case let viewController as ViewController:
+            restorationHandler([viewController])
+            
+        default :
+            print("do nothing")
+            
+        }
+        
+        return true
+    }
 
 
 }
--- HandoffContinuity/ViewController.swift
+++ HandoffContinuity/ViewController.swift
@@ -9,19 +9,48 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    @IBOutlet weak var titleTextField: NSTextField!
+    @IBOutlet var contentTextView: NSTextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
+    
+    override func viewDidAppear() {
+        createUserActivity()
+    }
+    
+    private func createUserActivity(){
+        let userActivity = NSUserActivity(activityType: "xyz.henriquevalcanaia.HandoffContinuity.notes.edit")
+        userActivity.title = "Title"
+        userActivity.addUserInfoEntriesFromDictionary(["title":titleTextField.stringValue, "content":contentTextView.string!])
+        self.userActivity = userActivity;
+        self.userActivity?.becomeCurrent()
+    }
+    
     override var representedObject: AnyObject? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
-
+    
+    override func updateUserActivityState(userActivity: NSUserActivity) {
+        userActivity.title = "Title"
+        userActivity.addUserInfoEntriesFromDictionary(["title":titleTextField.stringValue, "content":contentTextView.string!])
+    }
+    
+    deinit {
+        self.userActivity?.invalidate()
+    }
+    
+    override func restoreUserActivityState(userActivity: NSUserActivity) {
+        if let userInfo:[NSObject:AnyObject] = userActivity.userInfo{
+            titleTextField.stringValue = (userInfo["title"] as? String)!
+            contentTextView.string = userInfo["content"] as? String
+        }
+    }
+    
 }
 
